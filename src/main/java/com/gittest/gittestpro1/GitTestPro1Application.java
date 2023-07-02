e36407b128c88c19734dd4e26b7135825bbaf1ac
wanjun1999 <1457221049@qq.com>
2023-07-02 22:24:38 +0800

    first commit

---
+++ src/main/java/com/gittest/gittestpro1/GitTestPro1Application.java
@@ -0,0 +1,13 @@
+package com.gittest.gittestpro1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestPro1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestPro1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gittest/gittestpro1/GitTestPro1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gittest.gittestpro1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestPro1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
