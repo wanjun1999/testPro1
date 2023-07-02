d177d4aaa9dd10fcfd0cb3eb79cead70031f6a40
wanjun1999 <1457221049@qq.com>
2023-07-02 23:07:59 +0800

    hello commit

--- src/main/java/com/gittest/gittestpro1/HelloWorld.java
+++ src/main/java/com/gittest/gittestpro1/HelloWorld.java
@@ -3,5 +3,6 @@ package com.gittest.gittestpro1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("hello");
     }
 }
